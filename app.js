876645612970eadc8b018f3fddfc25eb39fe7169
matin <hsalehi91@gmail.com>
2018-10-03 21:41:25 -0400

    added sendGet and sendPost methods

--- app.js
+++ app.js
@@ -21,3 +21,31 @@ server.listen(PORT, HOST, function () {
 //ability to use POST and mapping request parametrs
 server.use(restify.plugins.fullResponse());
 server.use(restify.plugins.bodyParser());
+
+//sendGet  *** shows all the notes
+server.get('/sendGet', function (req, res, next) {
+    notesSaved.find({}, function (error, notes) {
+        res.send(notes);
+    })
+});
+
+//sendPost *** add a note: {id, name, text}
+server.post('/sendPost', function (req, res, next) {
+    //creating a note
+    var newNote = {
+        name: req.params.name,
+        text: req.params.text
+    };
+
+    //saving Note in memory, if error, display error, otherwise send Note info in body response
+    notesSaved.create(newNote, function (error, note) {
+        if (error) {
+            console.log("sendPost: Error creating a Note");
+            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
+        }
+        else {
+            res.send(201,note);
+            console.log("sendPost: Successfully Added Note");
+        }
+    });
+    });
