2a190d6a6ee3b3c565a23fe39b25c5fcf926f87c
matin <hsalehi91@gmail.com>
2018-10-03 22:38:16 -0400

    sendDelete method added, counters for get and post added, some logs added

--- app.js
+++ app.js
@@ -22,30 +22,61 @@ server.listen(PORT, HOST, function () {
 server.use(restify.plugins.fullResponse());
 server.use(restify.plugins.bodyParser());
 
+
+var getCount = 0;
+var postCount = 0;
+
+
 //sendGet  *** shows all the notes
 server.get('/sendGet', function (req, res, next) {
+    getCount++;
+    console.log("> sendGet: request received");
     notesSaved.find({}, function (error, notes) {
+        console.log("< sendGet: sending response");
         res.send(notes);
     })
+    console.log("Processed Request Counters ---> GET: %s | POST: %s", getCount, postCount);
 });
 
 //sendPost *** add a note: {id, name, text}
 server.post('/sendPost', function (req, res, next) {
+    postCount++;
+    console.log("> sendPost: request received");
     //creating a note
     var newNote = {
         name: req.params.name,
         text: req.params.text
     };
-
-    //saving Note in memory, if error, display error, otherwise send Note info in body response
     notesSaved.create(newNote, function (error, note) {
+        if (req.params.name === undefined ) {
+            console.log("sendPost: Error Note name must be entered!");
+            console.log("Processed Request Counters ---> GET: %s | POST: %s", getCount, postCount);
+            return next(new restify.InvalidArgumentError('Note name must be entered!'));
+        }
         if (error) {
             console.log("sendPost: Error creating a Note");
+            console.log("Processed Request Counters ---> GET: %s | POST: %s", getCount, postCount);
             return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
         }
         else {
             res.send(201,note);
             console.log("sendPost: Successfully Added Note");
         }
+    }); 
+    console.log("Processed Request Counters ---> GET: %s | POST: %s", getCount, postCount);
+});
+
+//sendDelete *** Delete all notes
+server.del('/sendDelete', function (req, res, next) {
+    console.log("> sendDelete: request received");
+    notesSaved.deleteMany({}, function (error, notes) {
+        if (error) {
+            console.log("sendDelete: Error");
+            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
+        }
+        else {
+            res.send(200);
+            console.log("sendDelete: Successfully Deleted all Notes");
+        }
     });
-    });
+});
